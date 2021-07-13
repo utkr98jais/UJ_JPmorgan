0c56bc4c02a9c411f0b9bbc6b2a36628971c934c
Utkarsh Jaiswal <utkr98.jais@gmail.com>
2021-07-13 10:42:46 +0530

    ylo

---
+++ pal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class pal {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int a ; String s;
+//        s = sc.nextLine();
+//        try {
+//            a = Integer.parseInt(s);
+//            System.out.println("It's a number");
+//        }
+//        catch (Exception e){
+//            System.out.println("It's a string!");
+//        }
+//            String temp = "";
+//            for (int i=s.length()-1; i>=0; i--){
+//                temp += s.charAt(i);
+//            }
+//            if ( temp.equalsIgnoreCase(s))
+//                System.out.println("Yes, Pallindrome");
+//            else
+//                System.out.println("Not a Pallindrome");
+        String s1= new String("Hello");
+        String s2 = s1;
+        s1 ="Bello";
+        System.out.println(s1 + " " + s2);
+
+    }
+}
